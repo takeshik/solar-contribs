b4789f7f87a899327d1bef867a355974b76b566b
Takeshi KIRIYA <takeshik@users.sf.net>
2010-12-21 08:34:02 +0900

    Add auto-start option in WebService, modify menu text

--- WebService.py
+++ WebService.py
@@ -12,6 +12,8 @@ from XSpect.Contributions.Solar import *
 WebService.Instance.Configuration.ListenAddress = "127.0.0.1"
 # listen するポート番号
 WebService.Instance.Configuration.ListenPort = 8080
+# 起動時に自動的に開始するかどうか
+WebService.Instance.Configuration.AutoStart = True
 
 ### 設定 ここまで
 
@@ -20,13 +22,15 @@ stopMenu = MenuItem()
 
 def Load():
     parentMenu = App.Current.MainWindow.MainMenu.Items[3]
-    startMenu.Header = 'Web サーバの開始'
+    startMenu.Header = 'Web サービスの開始'
     startMenu.Click += StartWebService
     parentMenu.Items.Add(startMenu)
-    stopMenu.Header = 'Web サーバの停止'
+    stopMenu.Header = 'Web サービスの停止'
     stopMenu.Click += StopWebService
     parentMenu.Items.Add(stopMenu)
     stopMenu.IsEnabled = False
+    if WebService.Instance.Configuration.AutoStart:
+        StartWebService(None, None)
 
 def StartWebService(sender, e):
     WebService.Instance.Start()
