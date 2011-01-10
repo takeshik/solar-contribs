119bb1f1f6da372681ca36afb97cbab2c0c09be4
Takeshi KIRIYA <takeshik@users.sf.net>
2011-01-11 00:04:52 +0900

    Add EventAction library

---
+++ EventAction/EventAction.cs
@@ -0,0 +1,40 @@
+﻿// -*- mode: csharp; encoding: utf-8; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
+// vim:set ft=cs fenc=utf-8 ts=4 sw=4 sts=4 et:
+// $Id$
+/* EventAction
+ *   Enables performing custom codes with real-time events of StatusCache
+ * Copyright © 2011 Takeshi KIRIYA (aka takeshik) <takeshik@users.sf.net>
+ * All rights reserved.
+ * 
+ * This file is part of WebService.
+ * 
+ * This library is dual-licensed, under the MIT License, and the same conditions
+ * which copyright holder of Solar prescribes. You can use this library and its
+ * source codes under the license you selected.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Concurrency;
+using System.Linq;
+using Lunar;
+using Solar.Models;
+using Ignition;
+
+namespace XSpect.Contributions.Solar
+{
+    public static class EventAction
+    {
+        public static IDisposable Register(dynamic condition, dynamic action)
+        {
+            return Observable.FromEvent<EventArgs<Category, IList<IEntry>>>(Client.Instance, "Refreshed")
+                .SelectMany(e => e.EventArgs.Value2 != null
+                    ? e.EventArgs.Value2.ToObservable()
+                    : Observable.Empty<IEntry>()
+                )
+                .Where(_ => condition(_))
+                .SubscribeOn(Scheduler.TaskPool)
+                .Subscribe(_ => action(_));
+        }
+    }
+}
---
+++ EventAction/Properties/AssemblyInfo.cs
@@ -0,0 +1,47 @@
+﻿// -*- mode: csharp; encoding: utf-8; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
+// vim:set ft=cs fenc=utf-8 ts=4 sw=4 sts=4 et:
+// $Id$
+/* EventAction
+ *   Enables performing custom codes with real-time events of StatusCache
+ * Copyright © 2011 Takeshi KIRIYA (aka takeshik) <takeshik@users.sf.net>
+ * All rights reserved.
+ * 
+ * This file is part of WebService.
+ * 
+ * This library is dual-licensed, under the MIT License, and the same conditions
+ * which copyright holder of Solar prescribes. You can use this library and its
+ * source codes under the license you selected.
+ */
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WebService")]
+[assembly: AssemblyDescription("Extensional library for Solar, real-time event reactiving mechanism")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("XSpect Project")]
+[assembly: AssemblyProduct("WebService")]
+[assembly: AssemblyCopyright("Copyright © 2011 Takeshi KIRIYA (aka takeshik) <takeshik@users.sf.net>, All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8f754853-3c87-4e51-bce3-20d47d8b35e8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
